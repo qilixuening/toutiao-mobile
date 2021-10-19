f9eb6a9a21e04fccacb677a870e236069b242709
qilixuening <qilixuening@hotmail.com>
2021-10-19 22:36:57 +0800

    完成登录布局

--- src/main.js
+++ src/main.js
@@ -3,12 +3,12 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-import './styles/index.less'
-
 import Vant from 'vant'
 import 'vant/lib/index.css'
 
 import 'amfe-flexible'
+
+import './styles/index.less'
 
 Vue.use(Vant)
 
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,5 +1,46 @@
 <template>
-  <div class="login-container">登录页面</div>
+  <div class="login-container">
+    <van-nav-bar
+      title="标题"
+      class="app-nav-bar"
+      left-arrow
+      @click-left="onClickLeft"
+    />
+    <van-cell-group>
+      <van-field
+        v-model="user.mobile"
+        left-icon="manager-o"
+        placeholder="请输入手机号"
+      />
+      <van-field
+        v-model="user.code"
+        clearable
+        left-icon="certificate"
+        placeholder="请输入验证码"
+      >
+        <template #button>
+          <van-button
+            size="small"
+            color="#3296fa"
+            plain
+            round
+          >
+            发送验证码
+          </van-button>
+        </template>
+      </van-field>
+    </van-cell-group>
+    <div class="login-button">
+      <van-button
+        size="normal"
+        type="info"
+        color="#3296fa"
+        block
+      >
+        登录/注册
+      </van-button>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -8,14 +49,27 @@ export default {
   components: {},
   props: {},
   data () {
-    return {}
+    return {
+      user: {
+        mobile: '',
+        code: ''
+      }
+    }
   },
   computed: {},
   watch: {},
   created () {},
   mounted () {},
-  methods: {}
+  methods: {
+    onClickLeft () {
+      this.$router.back()
+    }
+  }
 }
 </script>
 
-<style scoped lang="less"></style>
+<style scoped lang="less">
+.login-button {
+  padding: 26px 16px;
+}
+</style>
