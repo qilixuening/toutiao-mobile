dd91002e083c0ca32e1eed1cc0bc69f91084a2fe
qilixuening <qilixuening@hotmail.com>
2021-10-19 15:43:29 +0800

    完成项目初始化及基本配置

---
+++ src/views/layout/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="layout-container">
+    <router-view />
+    <van-tabbar v-model="currentTab" route>
+      <van-tabbar-item icon="wap-home-o" to="/">首页</van-tabbar-item>
+      <van-tabbar-item icon="comment-o" to="/qa">问答</van-tabbar-item>
+      <van-tabbar-item icon="video-o" to="/video">视频</van-tabbar-item>
+      <van-tabbar-item icon="user-o" to="/my">我的</van-tabbar-item>
+    </van-tabbar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LayoutIndex',
+  components: {},
+  props: {},
+  data () {
+    return {
+      currentTab: 0
+    }
+  },
+  computed: {},
+  watch: {},
+  created () {},
+  mounted () {},
+  methods: {}
+}
+</script>
+
+<style scoped lang="less"></style>
